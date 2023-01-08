bcca87c788691efc9eb6fff3d9820d913144a84d
empereur7 <empereur079@gmail.com>
2023-01-08 17:00:35 +0900

    add user table

--- src/main/java/com/example/its/config/SecurityConfig.java
+++ src/main/java/com/example/its/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.its.config;
 
-import com.example.its.domain.auth.CustomeUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,13 +12,21 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // for h2-console
+        http.authorizeHttpRequests().antMatchers("/h2-console/**").permitAll()
+            .and()
+            .csrf()
+            .ignoringAntMatchers("/h2-console/**")
+            .and()
+            .headers().frameOptions().disable();
+
         http
-                .authorizeHttpRequests()
-                .mvcMatchers("/login/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login");
+            .authorizeHttpRequests()
+            .mvcMatchers("/login/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin()
+            .loginPage("/login");
         return http.build();
     }
 
